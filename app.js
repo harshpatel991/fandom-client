c82fd9bbc5d5f0049a5e0c416b1e7cc55f818e86
Harsh <hpatel90@illinos.edu>
2015-05-09 21:45:20 -0500

    Undo that last commit

--- app.js
+++ app.js
@@ -3,17 +3,6 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-var allowCrossDomain = function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-	res.header("Access-Control-Allow-Headers", "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept", "Access-Control-Allow-Origin");
-	res.header("Allow-Credentials", true);
-	res.header("Access-Control-Allow-Credentials", true)
-	res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT");
-
-	next();
-};
-app.use(allowCrossDomain);
-
 var port = process.env.PORT || 3000;
 console.log("Express server running on " + port);
 app.listen(process.env.PORT || port);
